2757f197c12a92779e828f2169f1caed046bcd60
Karsen Keith <karsen.keith@gmail.com>
2019-02-28 14:59:03 -0600

    Generalized method for retrieving information added to all classes except users (pending structural split for the user class)

--- app.py
+++ app.py
@@ -2,6 +2,11 @@ from flask import Flask
 from flask_restful import Api
 from pymongo import MongoClient
 from resources.users import Users
+from resources.exercises import Exercises
+from resources.machines import Machines
+from resources.archives import Archives
+from resources.journals import Journals
+from resources.workouts import Workouts
 
 app = Flask(__name__)
 api = Api(app)
@@ -10,6 +15,7 @@ client = MongoClient("mongodb+srv://nicholas_tiner:senior_design@csce483-dn7uw.m
 db = client['wait-data']
 
 api.add_resource(Users, '/users/<string:email>', resource_class_kwargs={'db': db})
+api.add_resource(Exercises, '/exercises/<string:query_category>/<string:query_key>', resource_class_kwargs={'db': db})
 
 if __name__ == '__main__':
     #app.debug = True
--- resources/exercises.py
+++ resources/exercises.py
@@ -1,8 +1,30 @@
-from flask_restful import Resource, reqparse, abort
+from flask_restful import Resource # our class must be of type Resource
+from bson.objectid import ObjectId # needed to convert object id string back to type object id
 
 class Exercises(Resource):
+    # set the collection to exercises
     def __init__(self, **kwargs):
         self.db = kwargs['db']
         self.exercises = self.db['exercises']
-    def get(self):
-        return {'initial': 'commit'}, 200
+
+    # general get request to get exercise(s)
+    def get(self, query_category, query_key):
+
+        # adjust the types accordingly since default is string
+        if query_category == 'machine_type_id':
+            query_key = int(query_key)
+        if query_category == '_id':
+            query_key = ObjectId(query_key)
+
+        # in order to return a result needs to be {} format
+        result_cursor = self.exercises.find({query_category : query_key})
+        return_result = {}
+        for document in result_cursor:
+            document['_id'] = str(document['_id'])
+            return_result[document['_id']] = document
+
+        # if unable to find matching query item
+        if return_result == {}:
+            return_result = {'error:' : 'Not Found'}
+
+        return return_result
