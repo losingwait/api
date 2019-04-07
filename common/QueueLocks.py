d7420fa4c810c77902fa38a67dd1e3df38f62575
nick <ntiner1@tamu.edu>
2019-04-07 14:32:04 -0500

    Update queue locks to prevent deadlock

--- common/QueueLocks.py
+++ common/QueueLocks.py
@@ -6,18 +6,23 @@ class QueueLocks():
         self.lock = Lock()
 
     def lockQueue(self, machine_group_id):
+        # lock is required to two threads dont try and make the same lock
         self.lock.acquire()
         if machine_group_id not in self.locks:
             self.locks[machine_group_id] = Lock()
-        self.locks[machine_group_id].acquire()
         self.lock.release()
+        self.locks[machine_group_id].acquire()
         return True
 
     def unlockQueue(self, machine_group_id):
+        # lock is required to check conditional
         self.lock.acquire()
         if machine_group_id not in self.locks:
             self.lock.release()
             return False
-        self.locks[machine_group_id].release()
         self.lock.release()
+        try:
+            self.locks[machine_group_id].release()
+        except RuntimeError:
+            return False
         return True
